92ef0ea087877a19e1d24cda17bad63462878d43
umar9543 <115045882+umar9543@users.noreply.github.com>
2023-02-09 03:04:56 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+async function guessing() {
+    const num = Math.floor((Math.random() * 10) + 1);
+    await inquirer.prompt([
+        {
+            type: "number",
+            name: "userguess",
+            message: "guess the number:"
+        }
+    ])
+        // answers();
+        //const {userguess}=answers;
+        .then((answer) => {
+        console.log("system guess:", num);
+        if (answer.userguess === num) {
+            console.log("huray you win");
+        }
+        else {
+            console.log("try again");
+        }
+    });
+}
+async function start() {
+    do {
+        await guessing();
+        var again = await inquirer
+            .prompt({
+            type: "input",
+            name: "restart",
+            message: "press y for continue and n for leave:"
+        });
+    } while (again.restart == 'y' || again.restart == 'Y' || again.restart == 'yes' || again.restart == 'YES');
+}
+start();
